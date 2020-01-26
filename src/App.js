3888c8da056347fc9c61ebf65284e3b3eda46107
MartinGwon <anatomy1545@gmail.com>
2020-01-27 02:44:33 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Movies from "./Movie";
+import "./App.css";
+// import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  async componentWillMount() {
+    const movies = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies: movies.data.data.movies, isLoading: false });
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section class="container">
+        <h2>
+          {isLoading ? (
+            <div class="loader">
+              <span class="loader__text">Loading...</span>
+            </div>
+          ) : (
+            movies.map(potato => {
+              return (
+                <div class="movies">
+                  <Movies
+                    key={potato.id}
+                    id={potato.id}
+                    genres={potato.genres}
+                    title={potato.title}
+                    summary={potato.summary}
+                    poster={potato.medium_cover_image}
+                    year={potato.year}
+                  />
+                </div>
+              );
+            })
+          )}
+        </h2>
+      </section>
+    );
+  }
 }
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+function Movie({ title, summary, poster, year, genres }) {
+  return (
+    <div class="movie">
+      <div class="movie__movie-title">{title}</div>
+      <div class="movie__movie-year">({year})</div>
+      <ul class="movie__movie-genres">
+        {genres.map((tomato, index) => (
+          <li key={index} class="movie__movie-genres-name">
+            {tomato}
+          </li>
+        ))}
+      </ul>
+      <div class="poster-container">
+        <img
+          src={poster}
+          alt={title}
+          title={title}
+          class="movie__movie-poster"
+        ></img>
+        <p class="movie__movie-summary">{summary}</p>
+      </div>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string)
+};
+
+export default Movie;
