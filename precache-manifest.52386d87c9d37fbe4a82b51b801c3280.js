86bb09c47fe728a2c2518916983768fc64cf142e
MartinGwon <anatomy1545@gmail.com>
2020-01-27 03:27:43 +0900

    Updates

---
+++ precache-manifest.52386d87c9d37fbe4a82b51b801c3280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27573ba30a7959eb184c7ddfe67a50b",
+    "url": "/react_practice-1/index.html"
+  },
+  {
+    "revision": "f4acac403f7978d295c6",
+    "url": "/react_practice-1/static/css/main.f02b3dab.chunk.css"
+  },
+  {
+    "revision": "9896895daac2904c73d8",
+    "url": "/react_practice-1/static/js/2.411d4022.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_practice-1/static/js/2.411d4022.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4acac403f7978d295c6",
+    "url": "/react_practice-1/static/js/main.e3b0c0ee.chunk.js"
+  },
+  {
+    "revision": "f22d2d54704968ebde4e",
+    "url": "/react_practice-1/static/js/runtime-main.1f624c47.js"
+  }
+]);
